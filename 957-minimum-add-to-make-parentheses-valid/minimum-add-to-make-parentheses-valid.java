58708db116fd048228812bba62de97f3ffae6ed9
piyushdhonge71-cmd <piyushdhonge71@gmail.com>
2026-09-15 23:07:47 +0530

    Time: 3 ms (4.97%) | Memory: 42.6 MB (94.77%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stack.push(ch);
+            }
+            else {
+                
+                if(stack.size() != 0 && stack.peek() == '('){
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+               
+
+            }
+        }
+         return stack.size();
+        
+    }
+}
